1ab9a427134a4dfb3a0e1052a8c05ef42fb599ae
NidhitheCoder <nidhpv@gmail.com>
2020-09-26 23:54:13 +0530

    add monster as childrens

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {CardList}  from './components/card-list/card-list.component';
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -10,22 +10,20 @@ class App extends Component {
     };
   }
 
-
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState({monsters:users}))
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-      <CardList name="Smith">
-      <h1>July</h1>
-      </CardList>
+        <CardList name="Smith">
         {this.state.monsters.map(monster => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
+        </CardList>
       </div>
     );
   }
