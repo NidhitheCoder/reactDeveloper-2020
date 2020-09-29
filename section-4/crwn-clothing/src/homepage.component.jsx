31b758c33588b2c46f85c06affaf929ea9000b11
NidhitheCoder <nidhpv@gmail.com>
2020-09-29 16:50:54 +0530

    styles added

--- section-4/crwn-clothing/src/homepage.component.jsx
+++ section-4/crwn-clothing/src/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className="homepage">
@@ -24,7 +25,7 @@ const HomePage = () => (
         </div>
       </div>
 
-      <div className="memu-item">
+      <div className="menu-item">
         <div className="content">
           <h1 className="title">WOMENS</h1>
           <span className="subtitle">Shop Now</span>
