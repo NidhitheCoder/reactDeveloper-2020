0dfdc91df325c9942f848c8f5dc9768d8ad4f467
NidhitheCoder <nidhpv@gmail.com>
2020-09-30 12:15:51 +0530

    3 pages created

--- section-4/simple-router-app/src/App.js
+++ section-4/simple-router-app/src/App.js
@@ -1,9 +1,41 @@
 import React from "react";
-import "./App.css";
+import { Route } from "react-router-dom";
+
+const HomePage = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+const TopicList = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topic List</h1>
+    </div>
+  );
+};
+
+const TopicDetails = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topic Details Page : {props.match.params.topicId}</h1>
+    </div>
+  );
+};
 
 function App() {
-  return <div className="App">
-  </div>;
+  return (
+    <div>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/topics" component={TopicList} />
+      <Route path="/topics/:topicId" component={TopicDetails} />
+    </div>
+  );
 }
 
 export default App;
