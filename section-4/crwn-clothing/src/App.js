ba6fff9883252a897464f1afea23293b17d0268d
NidhitheCoder <nidhpv@gmail.com>
2020-09-30 17:54:48 +0530

    add sign in component and signin-and-signout page

--- section-4/crwn-clothing/src/App.js
+++ section-4/crwn-clothing/src/App.js
@@ -4,6 +4,7 @@ import {Switch,Route} from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shopPage/shopPage.component";
 import Header from './components/header/header.component';
+import SignInAndSignUp  from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 // const HatPage = () =>(
 //   <div>
@@ -17,6 +18,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path="/shop" component={ShopPage} />
+      <Route path="/signin" component={SignInAndSignUp} />
     </Switch>
     </div>
   );
