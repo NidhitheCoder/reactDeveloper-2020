2dd659d7513b0d1f00d465b4055cbd311ac691b0
NidhitheCoder <nidhpv@gmail.com>
2020-09-30 12:43:35 +0530

    add link component

--- section-4/simple-router-app/src/App.js
+++ section-4/simple-router-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 const HomePage = props => {
   console.log(props);
   return (
     <div>
+      <Link to="/topics">Topics</Link>
+      <button onClick={() => props.history.push("/topics")}>Topics</button>
       <h1>Home Page</h1>
     </div>
   );
@@ -14,6 +16,9 @@ const TopicList = props => {
   console.log(props);
   return (
     <div>
+      <Link to={`${props.match.url}/13`}>To Topic 13</Link>
+      <Link to={`${props.match.url}/14`}>To Topic 14</Link>
+      <Link to={`${props.match.url}/15`}>To Topic 15</Link>
       <h1>Topic List</h1>
     </div>
   );
