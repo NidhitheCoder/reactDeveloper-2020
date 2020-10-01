37fe69242532e1d62fecbae1deeb121eec225efd
NidhitheCoder <nidhpv@gmail.com>
2020-10-01 12:23:59 +0530

    funcitonal component  'App' changed as class component

--- section-4/crwn-clothing/src/App.js
+++ section-4/crwn-clothing/src/App.js
@@ -1,27 +1,51 @@
 import React from "react";
 import "./App.css";
-import {Switch,Route} from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shopPage/shopPage.component";
-import Header from './components/header/header.component';
-import SignInAndSignUp  from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
+import {auth} from './firebase/firebase.utlis';
 
 // const HatPage = () =>(
 //   <div>
 //   <h1>HATS PAGE</h1></div>
 // )
 
-function App() {
-  return (
-    <div>
-    <Header />
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-      <Route path="/signin" component={SignInAndSignUp} />
-    </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser : null
+    }
+  }
+
+  unSubscribeFromAuth = null;
+
+  componentDidMount(){
+   this.unSubscribeFromAuth = auth.onAuthStateChanged(user =>{
+      this.setState({currentUser:user });
+      console.log(user);
+    })
+  };
+
+  componentWillUnmount(){
+    this.unSubscribeFromAuth();
+  }
+  
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
