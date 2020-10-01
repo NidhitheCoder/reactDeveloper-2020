7efb0cfbcbc4b3af79c73144692f4fcd8a214f6c
NidhitheCoder <nidhpv@gmail.com>
2020-10-01 10:51:49 +0530

    Custom button component added

--- section-4/crwn-clothing/src/components/header/header.component.jsx
+++ section-4/crwn-clothing/src/components/header/header.component.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import './header.styles.scss';
-import {ReactComponent as Logo} from '../../assets/crown.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.styles.scss";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 const Header = () => (
-     <div className="header">
-        <Link className="logo-container" to="/">
-             <Logo className="logo" />    
-        </Link>
-        <div className="options">
-        <Link className="option" to="/shop">SHOP</Link>
-        <Link className="option" to="">CONTACT</Link>
-        <Link className="option" to="">ABOUT</Link>
-        </div>
-     </div>
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="">
+        CONTACT
+      </Link>
+      <Link className="option" to="/signin">
+        SIGN IN
+      </Link>
+    </div>
+  </div>
 );
 
 export default Header;
--- section-4/crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ section-4/crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./sign-in.styles.scss";
-import  FormInput from '../form-input/form-input.component';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component {
   constructor() {
@@ -45,7 +46,7 @@ class SignIn extends React.Component {
             value={this.state.password}
             required
           />
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit">Sign In</CustomButton>
         </form>
       </div>
     );
