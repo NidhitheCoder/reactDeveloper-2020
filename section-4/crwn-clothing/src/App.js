e495709db0cd8dc189f7a6b8f7de4e988f74a93e
NidhitheCoder <nidhpv@gmail.com>
2020-10-01 13:09:50 +0530

    google sign in button  styling

--- section-4/crwn-clothing/src/App.js
+++ section-4/crwn-clothing/src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shopPage/shopPage.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
-import {auth} from './firebase/firebase.utlis';
+import { auth } from "./firebase/firebase.utlis";
 
 // const HatPage = () =>(
 //   <div>
@@ -13,31 +13,30 @@ import {auth} from './firebase/firebase.utlis';
 // )
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      currentUser : null
-    }
+      currentUser: null
+    };
   }
 
   unSubscribeFromAuth = null;
 
-  componentDidMount(){
-   this.unSubscribeFromAuth = auth.onAuthStateChanged(user =>{
-      this.setState({currentUser:user });
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
       console.log(user);
-    })
-  };
+    });
+  }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unSubscribeFromAuth();
   }
-  
+
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
