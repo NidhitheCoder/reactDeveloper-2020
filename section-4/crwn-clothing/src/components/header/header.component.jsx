308b64a1256dbe5b90aaacf906bdcf5298c9b211
NidhitheCoder <nidhpv@gmail.com>
2020-10-03 16:18:05 +0530

    Add cart component

--- section-4/crwn-clothing/src/components/header/header.component.jsx
+++ section-4/crwn-clothing/src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./header.styles.scss";
 import { connect } from "react-redux";
+import  CartIcon from '../cart-icon/cart-icon.component';
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utlis";
 
@@ -26,6 +27,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
--- section-4/crwn-clothing/src/redux/user/user.action.js
+++ section-4/crwn-clothing/src/redux/user/user.action.js
@@ -1,6 +1,6 @@
-import {UserActionTypes} from './user.type';
+import { UserActionTypes } from "./user.type";
 
 export const setCurrentUser = user => ({
-    type: UserActionTypes.SET_CURRENT_USER,
-    payload:user
-})
+  type: UserActionTypes.SET_CURRENT_USER,
+  payload: user
+});
