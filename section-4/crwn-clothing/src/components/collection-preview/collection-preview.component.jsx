d8c52f58cf0f650735a0f6acaf84147c17cf0f28
NidhitheCoder <nidhpv@gmail.com>
2020-10-05 10:31:15 +0530

    add custom button to the collection items and add styles

--- section-4/crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ section-4/crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -5,8 +5,8 @@ import CollectionItem from "../collection-item/collection-item.component";
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
-      <h1 class="title">{title.toUpperCase()}</h1>
-      <div class="preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
           .map(({ id, ...otherItemProps }) => {
