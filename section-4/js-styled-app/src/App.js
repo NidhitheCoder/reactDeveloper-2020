e05569308a12e449ea9ff7c363b4e08970a0766f
NidhitheCoder <nidhpv@gmail.com>
2020-10-08 12:14:45 +0530

    styles using styled-components library

--- section-4/js-styled-app/src/App.js
+++ section-4/js-styled-app/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+import "./App.css";
+import { Card } from "./components/card/card.component";
+
+const Text = styled.div`
+  color: red;
+  font-size: 24px;
+  border: ${({ isActive }) =>
+    isActive ? "1px solid black" : "3px dotted green"};
+`;
+
+const textStyles = {
+  color: "red",
+  fontSize: "24px"
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        <div style={textStyles}> I am a component</div>
+        <Text isActive={false}>
+          I am a div component in the styled with in the name 'Text'
+        </Text>
+      </Card>
     </div>
   );
 }
