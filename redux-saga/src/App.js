98f7fa934cb52fa465716b6c0e70b898cd37a58e
NidhitheCoder <nidhpv@gmail.com>
2020-11-18 16:11:51 +0530

    basic working react app with redux

---
+++ redux-saga/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CounterContainer from './components/counter-container.component';
+
+const App = () =>  {
+  return (
+    <div className="App">
+      <CounterContainer />
+    </div>
+  );
+}
+
+export default App;
